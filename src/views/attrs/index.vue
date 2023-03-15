8f8a4b52d4de4654bc694a1441b0a35f79a343d6
牧云 <muyun@tsign.cn>
2023-03-15 19:59:31 +0800

    update

---
+++ src/views/attrs/index.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+     <el-button readonly disabled>我是按钮</el-button>
+     <el-button readonly disabled />
+   </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
